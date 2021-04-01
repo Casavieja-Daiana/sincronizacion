268e8294617ececc03079b11408125bd9b2d4905
Daiana <casaviejadaiana@gmail.com>
2021-04-01 11:01:09 -0300

    Modificando localmente el archivo nav

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-
+<nav>
+  <ol>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Quienes somos</a></li>
+    <li><a href="#">Nosotros</a></li>
+    <li><a href="#">Contactos</a></li>
+  </ol>
+</nav>
